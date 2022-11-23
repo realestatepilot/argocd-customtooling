cb8343e28f66bcd7607b2511b81ce728ba793e94
Thomas Grünert <thomas.gruenert@evermind.de>
2022-11-23 12:57:39 +0100

    debugging soap-wrapper

--- docker/sops-wrapper.sh
+++ docker/sops-wrapper.sh
@@ -3,12 +3,9 @@
 # script is called becauese variable HELM_SECRETS_SOPS_PATH is set
 echo "run sops-wrapper" >> /tmp/sops-wrapper.log
 
-if [ -z VAULT_ADDR ] 
+if [ -n $VAULT_ADDR ] 
 then
   export VAULT_TOKEN=$(cat /tmp/.vault_token)
 fi
-
-printenv >> /proc/1/fd/1 2>&1 &
-printenv >> /tmp/sops-wrapper.log
 
 sops $@
