4feebc1d661abb1e5675f7178abc70e39a8fec7e
Thomas Grünert <thomas.gruenert@evermind.de>
2022-11-23 12:29:03 +0100

    debugging soap-wrapper

--- docker/sops-wrapper.sh
+++ docker/sops-wrapper.sh
@@ -2,7 +2,11 @@
 # paste VAULT_TOKEN in environment
 # script is called becauese variable HELM_SECRETS_SOPS_PATH is set
 echo "run sops-wrapper" >> /tmp/sops-wrapper.log
-source ~/.bashrc
+# source ~/.bashrc
+
+if [[ -n "${VAULT_ADDR}"]]; then
+  export VAULT_TOKEN=$(cat /tmp/.vault_token)
+fi
 
 printenv >> /proc/1/fd/1 2>&1 &
 printenv >> /tmp/sops-wrapper.log
