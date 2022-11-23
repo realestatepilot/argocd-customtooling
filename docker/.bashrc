39b97c32ee9820455a29069f38e11e14eb0289e3
Thomas Grünert <thomas.gruenert@evermind.de>
2022-11-23 11:42:41 +0100

    debug sops-wrapper

--- docker/.bashrc
+++ docker/.bashrc
@@ -1,3 +1,5 @@
+echo "run .bashrc" >> /tmp/sops-wrapper.log
+
 FILE=/etc/vault.d/roleid
 if [ -f "$FILE" ]; then
     export VAULT_TOKEN=$(cat /tmp/.vault_token)
--- docker/sops-wrapper.sh
+++ docker/sops-wrapper.sh
@@ -1,8 +1,10 @@
 #/bin/bash
 # paste VAULT_TOKEN in environment
 # script is called becauese variable HELM_SECRETS_SOPS_PATH is set
+echo "run sops-wrapper" >> /tmp/sops-wrapper.log
 source ~/.bashrc
 
 printenv >> /proc/1/fd/1 2>&1 &
+printenv >> /tmp/sops-wrapper.log
 
 sops $@
