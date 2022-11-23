b7e436b0d0782dc8f850808577b3baa6f50049fd
Thomas Grünert <thomas.gruenert@evermind.de>
2022-11-23 13:19:48 +0100

    cleanup sops-wrapper.sh

--- docker/sops-wrapper.sh
+++ docker/sops-wrapper.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 # paste VAULT_TOKEN in environment
 # script is called becauese variable HELM_SECRETS_SOPS_PATH is set
-echo "run sops-wrapper" >> /tmp/sops-wrapper.log
 
 if [ -n $VAULT_ADDR ] 
 then
   export VAULT_TOKEN=$(cat /tmp/.vault_token)
 fi
-
-printenv | grep "VAULT" >> /proc/1/fd/1 2>&1 &
-printenv | grep "VAULT" >> /tmp/sops-wrapper.log
 
 sops $@
